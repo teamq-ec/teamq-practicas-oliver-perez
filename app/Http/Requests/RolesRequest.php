a892c91653f49c9a9fd9ec0e24649dab0a5a001a
OliverPerezyavirac <ods.perez@yavirac.edu.ec>
2023-07-24 00:10:27 -0500

    Cambio de atributo a name en ROL

--- app/Http/Requests/RolesRequest.php
+++ app/Http/Requests/RolesRequest.php
@@ -22,7 +22,7 @@ class RolesRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'rol_usuario'=> 'required|string',
+            'nombre'=> 'required|string',
         ];
     }
 
