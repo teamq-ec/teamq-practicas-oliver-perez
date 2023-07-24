32338c0d944e471b6746ea2daf8d4c3b0dfe1b58
OliverPerezyavirac <ods.perez@yavirac.edu.ec>
2023-07-23 23:58:33 -0500

    Api resource aplicada a rutas User y Roles

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,8 @@
 <?php
-
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\PictureController;
 use App\Http\Controllers\RolesController;
 use App\Http\Controllers\UsuarioController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,28 +17,16 @@ use Illuminate\Support\Facades\Route;
 */
 
 // AUTH LOGIN OF USER
-Route::post('/login', [UsuarioController::class, 'login']);
+Route::post('/login', [LoginController::class, 'login']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
     // RUTAS PARA USUARIOS 
-    Route::get   ('/usuarios',      [UsuarioController::class, 'index']);
-    Route::get   ('/usuarios/{id}', [UsuarioController::class, 'show']);
-    Route::post  ('/usuarios',      [UsuarioController::class, 'create']);
-    Route::put   ('/usuarios/{id}', [UsuarioController::class, 'update']);
-    Route::patch ('/usuarios/{id}', [UsuarioController::class, 'edit']);
-    Route::delete('/usuarios/{id}', [UsuarioController::class, 'destroy']);
-    
-    // RUTAS PARA IMAGENES
-    Route::get   ('/usuarios/{idusuarios}/picture', [UsuarioController::class, 'getPicture']);
-    Route::put   ('/usuarios/{idusuarios}/picture', [UsuarioController::class, 'updateOnePicture']);
-    Route::delete('/usuarios/{idusuarios}/picture', [UsuarioController::class, 'deletePicture']);
-    Route::post  ('/usuarios/{idusuarios}/picture', [UsuarioController::class, 'uploadPicture']);
+    Route::apiResource('/usuarios', UsuarioController::class);
     
     // RUTAS PARA ROLES
-    Route::get   ('/rol',      [RolesController::class, 'index']);
-    Route::get   ('/rol/{id}', [RolesController::class, 'show']);
-    Route::post  ('/rol',      [RolesController::class, 'create']);
-    Route::put   ('/rol/{id}', [RolesController::class, 'update']);
-    Route::patch ('/rol/{id}', [RolesController::class, 'edit']);
-    Route::delete('/rol/{id}', [RolesController::class, 'destroy']);
+    Route::apiResource ('/roles', RolesController::class);
+    // RUTAS PERSONALIZADAS
+    Route::get   ('/usuarios/{idusuarios}/picture', [PictureController::class, 'getPicture']);
+    Route::delete('/usuarios/{idusuarios}/picture', [PictureController::class, 'deletePicture']);
+    Route::post  ('/usuarios/{idusuarios}/picture', [PictureController::class, 'uploadPicture']);
 });
